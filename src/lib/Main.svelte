d6e51e1b4ac96d40257220cb873963e7817b7da1
Erik <78821053+swedishfrenchpress@users.noreply.github.com>
2025-07-22 14:30:23 +0200

    updated main page to begin new UI styling.

--- src/lib/Main.svelte
+++ src/lib/Main.svelte
@@ -1,179 +1,31 @@
 <script lang="ts">
-  import { Toaster } from "svelte-sonner";
   import Step1 from "./Step1.svelte";
   import Step2 from "./Step2.svelte";
   import Step3 from "./Step3.svelte";
   import Step4 from "./Step4.svelte";
   import { step } from "./stores.svelte";
-  import { Bitcoin, Code, Copy, Heart, Zap } from "lucide-svelte";
-    import { copyTextToClipboard } from "./utils";
-    import DonateCashu from "./DonateCashu.svelte";
-    let showDonate = $state(false)
 </script>
 
-<Toaster richColors position="top-right"></Toaster>
-<div
-  class="h-full min-h-screen w-screen bg-gradient-to-br from-primary to-secondary items-center flex justify-center flex-col p-2"
->
-  <div
-    class="flex m-3 mb-3 gap-2 flex-col items-center bg-base-100 w-full max-w-6xl h-full p-5 rounded-lg shadow-md"
-  >
-    <h1 class="w-full text-center font-bold text-2xl">
-      Money printer go brrrrr
+<div class="min-h-screen w-full bg-yellow-300 px-32">
+  <div class="w-full py-8 mb-6">
+    <h1 class="text-center font-ojuju font-bold text-4xl text-gray-900">
+      Print your own Ecash notes
     </h1>
-    <ul class="steps">
-      <li data-content="🏦" class="step step-primary"></li>
-      <li data-content="💵" class="step {$step > 1 ? 'step-primary' : ''}"></li>
-      <li data-content="⚡" class="step {$step > 2 ? 'step-primary' : ''}"></li>
-      <li data-content="🖨️" class="step {$step > 3 ? 'step-primary' : ''}"></li>
-    </ul>
-    <div class="m-auto w-full">
-      {#if $step === 1}
-        <Step1 />
-      {:else if $step === 2}
-        <Step2 />
-      {:else if $step === 3}
-        <Step3 />
-      {:else}
-        <Step4 />
-      {/if}
-    </div>
-    <div class="flex gap-4">
-      <button
-        class="m-4 btn btn-sm btn-error flex"
-      onclick={()=>showDonate = !showDonate}
-        >
-        <Heart>
-
-        </Heart>
-        {#if showDonate}
-          Hide
-        {:else}
-          
-        Donate
-        {/if}
-      </button>
-      <a
-        href="https://github.com/gandlafbtc/cashu-brrr"
-        class="m-4 btn btn-sm btn-info "
-      >
-       <Code></Code>
-       Code
-      </a>
-    </div>
-    {#if showDonate}
-    
-    <div class="flex flex-col gap-2 flex-wrap">
-      <div class="flex flex-col gap-2 w-80">
-
-        <p>
-          Hi anon! If you appreciate the money printer, consider donating some sats. Value4Value!
-        </p>
-        <p>
-          Let's keep the money printer running.
-        </p>
-        <p>
-          Brrrrrrrrrrrrrrrrr...
-        </p>
-      </div>
-      <div class="flex gap-2 items-center">
-        <DonateCashu></DonateCashu>
-      </div>
-      <div class="flex gap-2 items-center">
-        <a
-              href="https://geyser.fund/project/brrr"
-              target="_blank"
-              rel="nofollow noopener noreferrer"
-              class="rounded-full w-40 bg-[#2dd4bf] bg-opacity-35 hover:bg-opacity-100 transition-all"
-              >
-              <img src="/geyser.svg" alt="geyser" />
-            </a>
-            <button>
-              <Copy class='w-5 h-5' onclick={()=> copyTextToClipboard('https://geyser.fund/project/brrr')}></Copy>
-            </button>
-            <p class="w-24 overflow-clip text-ellipsis text-xs text-nowrap">
-              https://geyser.fund/project/brrr
-            </p>
-          </div>
-          <div class="flex gap-2 items-center">
-            <a
-            class="p-2 flex items-center gap-1 rounded-full bg-blue-600 w-40 bg-opacity-35 hover:bg-opacity-100 transition-all"
-            href="bitcoin:+glisteningstruggle62"
-            >
-            <Bitcoin></Bitcoin>
-            <p class="text-xs">PayNym</p>
-          </a>
-          <button>
-            <Copy class='w-5 h-5' onclick={()=> copyTextToClipboard('+glisteningstruggle62')}></Copy>
-          </button>
-          <p class="w-24 overflow-clip text-ellipsis text-xs break-all">
-            +glisteningstruggle62
-          </p>
-        </div>
-        <div class="flex gap-2 items-center">
-          <a
-          class="p-2 flex items-center gap-1 rounded-full bg-blue-600 w-40 bg-opacity-35 hover:bg-opacity-100 transition-all"
-          
-          href="bitcoin:PM8TJQQMZMvfVGsJ2bJmbtDnx2BVgcPThnF8XS9ApGvuzYDPWirVuMencyhvK8WBDTgeaLLEoGEYggTMJFwjDkW8HhfqijrYHStMiFhKfA7UYbwXqYB6"
-          >
-          <Bitcoin></Bitcoin>
-          <p class="text-xs">Payment code</p>
-        </a>
-        <button>
-          <Copy class='w-5 h-5' onclick={()=> copyTextToClipboard('PM8TJQQMZMvfVGsJ2bJmbtDnx2BVgcPThnF8XS9ApGvuzYDPWirVuMencyhvK8WBDTgeaLLEoGEYggTMJFwjDkW8HhfqijrYHStMiFhKfA7UYbwXqYB6')}></Copy>
-        </button>
-        <p class="w-24 overflow-clip text-ellipsis text-xs text-nowrap">
-          PM8TJQQMZMvfVGsJ2bJmbtDnx2BVgcPThnF8XS9ApGvuzYDPWirVuMencyhvK8WBDTgeaLLEoGEYggTMJFwjDkW8HhfqijrYHStMiFhKfA7UYbwXqYB6
-        </p>
-      </div>
-      <div class="flex gap-2 items-center">
-        <a
-        class="p-2 flex items-center gap-1 rounded-full bg-orange-600 w-40 bg-opacity-35 hover:bg-opacity-100 transition-all"
-        
-        href="bitcoin:bc1q8sy2kd4c907r5y05wwghldpvvnkapkn7yrvs2u"
-        >
-        <Bitcoin></Bitcoin>
-        <p class="text-xs">Onchain address</p>
-      </a>
-      <button>
-        <Copy class='w-5 h-5' onclick={()=> copyTextToClipboard('bc1q8sy2kd4c907r5y05wwghldpvvnkapkn7yrvs2u')}></Copy>
-      </button>
-      <p class="w-24 overflow-clip text-ellipsis text-xs">
-        bc1q8sy2kd4c907r5y05wwghldpvvnkapkn7yrvs2u
-      </p>
-    </div>
-    <div class="flex gap-2 items-center">
-      <a
-      class="p-2 flex items-center gap-1 rounded-full bg-yellow-600 w-40 bg-opacity-35 hover:bg-opacity-100 transition-all"
-      
-      href="lightning:lno1zrxq8pjw7qjlm68mtp7e3yvxee4y5xrgjhhyf2fxhlphpckrvevh50u0q0pdmt47mvjvq77n7thpvj9m3hcppl4hu95ue953uq46ej46el9rxqsznmjm9j44w9jntmpsq9eec9002pmyjjf6w75v88dn4472zmctcdxqqvewfj3vxyqv54q4gzy7v60u27kgwnurfzpykxnwz0jr4gzg4swld8pglcv7rlulaemm665l4pmftckhj6vwq2e6l4zk8xc53rgt7dyfdd9j86pgfsgmhx67v4a64lfeqggchfm0uqqs6jz4x5lxj3f6qw7vxe4fg8s4z5"
-      >
-      <Zap></Zap>
-      <p class="text-xs">Bolt 12</p>
-    </a>
-    <button>
-      <Copy class='w-5 h-5' onclick={()=> copyTextToClipboard('lno1zrxq8pjw7qjlm68mtp7e3yvxee4y5xrgjhhyf2fxhlphpckrvevh50u0q0pdmt47mvjvq77n7thpvj9m3hcppl4hu95ue953uq46ej46el9rxqsznmjm9j44w9jntmpsq9eec9002pmyjjf6w75v88dn4472zmctcdxqqvewfj3vxyqv54q4gzy7v60u27kgwnurfzpykxnwz0jr4gzg4swld8pglcv7rlulaemm665l4pmftckhj6vwq2e6l4zk8xc53rgt7dyfdd9j86pgfsgmhx67v4a64lfeqggchfm0uqqs6jz4x5lxj3f6qw7vxe4fg8s4z5')}></Copy>
-    </button>
-    <p class="w-24 overflow-clip text-ellipsis text-xs">
-      lno1zrxq8pjw7qjlm68mtp7e3yvxee4y5xrgjhhyf2fxhlphpckrvevh50u0q0pdmt47mvjvq77n7thpvj9m3hcppl4hu95ue953uq46ej46el9rxqsznmjm9j44w9jntmpsq9eec9002pmyjjf6w75v88dn4472zmctcdxqqvewfj3vxyqv54q4gzy7v60u27kgwnurfzpykxnwz0jr4gzg4swld8pglcv7rlulaemm665l4pmftckhj6vwq2e6l4zk8xc53rgt7dyfdd9j86pgfsgmhx67v4a64lfeqggchfm0uqqs6jz4x5lxj3f6qw7vxe4fg8s4z5
-    </p>
   </div>
-  <div class="flex gap-2 items-center">
-    <a
-    class="p-2 flex items-center gap-1 rounded-full bg-yellow-600 w-40 bg-opacity-35 hover:bg-opacity-100 transition-all"
-    href="lightning:nutstashapp@geyser.fund"
-    >
-    <Zap></Zap>
-    <p class="text-xs">Lightning address</p>
-  </a>
-  <button>
-    <Copy class='w-5 h-5'onclick={()=> copyTextToClipboard('nutstashapp@geyser.fund')}></Copy>
-  </button>
-  <p class="w-24 overflow-clip text-ellipsis text-xs">
-    nutstashapp@geyser.fund
-  </p>
-</div>
-</div>
-{/if}
-</div>
+
+  <div class="m-auto w-full">
+    {#if $step === 1}
+      <Step1 />
+    {:else if $step === 2}
+      <Step2 />
+    {:else if $step === 3}
+      <Step3 />
+    {:else}
+      <Step4 />
+    {/if}
+  </div>
+
+  <div class="flex gap-4 justify-center mt-8">
+    <!-- Action buttons or other controls can go here -->
+  </div>
 </div>
